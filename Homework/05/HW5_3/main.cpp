6bfb4c19778d506aebd147ac7405db95be66ad6a
JoinedSenses <joinedsenses@gmail.com>
2019-06-19 11:03:40 -0400

    Create main.cpp

---
+++ Homework/05/HW5_3/main.cpp
@@ -0,0 +1,32 @@
+/*     ____   __     __
+        /   /   \  /   \
+       /   /      /
+      /    \___/  \___/
+ Name: Arron Vinyard
+ Date: June 19, 2019
+ Project Description: Prints a range of characters based on input
+ Inputs: Two characters
+ Outputs: Range of characters between two chars, 10 per line
+**************************************************************/
+#include <iostream>
+using namespace std;
+
+void printChars(char, char, int = 10);
+
+int main() {
+	char ch1, ch2;
+	cout << "Enter two characters to print a range of characters between the two:" << endl;
+	cin >> ch1 >> ch2;
+	printChars(ch1, ch2);
+	return 0;
+}
+
+void printChars(char ch1, char ch2, int numberPerLine) {
+	int count = 0;
+	while (ch1 <= ch2) {
+		cout << ch1++;
+		if (++count % numberPerLine == 0) {
+			cout << endl;
+		}
+	}
+}
