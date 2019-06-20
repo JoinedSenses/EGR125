e12e1e3ea13dd9af554703c1c8e0dabeb068b597
JoinedSenses <joinedsenses@gmail.com>
2019-06-19 20:06:32 -0400

    Update main.cpp

--- Homework/04/HW4_3C/main.cpp
+++ Homework/04/HW4_3C/main.cpp
@@ -13,24 +13,14 @@
 #include <iomanip>
 using namespace std;
 
-// ==== Conversion functions
-double mileToKm(double mile) {
-	return mile * 1.609;
-}
-
-double kmToMile(double km) {
-	return km / 1.609;
-}
-// ====
-
 int main() {
 	int i, j;
 	cout << fixed << setprecision(3) << left
 		<< "Miles  Kilometers | Kilometers  Miles\n";
 	// Output two formatted conversion tables, side by side
 	for (i = 1, j = 20; i < 11; i++, j+=5) {
-		cout << setw(7) << i << setw(11) << mileToKm(i) << "| "
-			<< setw(12) << j << kmToMile(j) << endl;
+		cout << setw(7) << i << setw(11) << i * 1.609 << "| "
+			<< setw(12) << j << j / 1.609 << endl;
 	}
 	return 0;
 }
